58644b078dcded718b5da5914b04be98907262d5
darko_bubnjarko <darko.jukic1991@gmail.com>
2017-03-07 13:26:13 +0100

    Removed unnecessary autogenerated properities in VehicleDbContext.

--- VehicleApp.Service/Models/VehicleDbContext.cs
+++ VehicleApp.Service/Models/VehicleDbContext.cs
@@ -7,9 +7,5 @@ namespace VehicleApp.Service.Models
         public VehicleDbContext() : base("name = VehicleDbContext") { }
         public DbSet<VehicleMake> Makes { get; set; }
         public DbSet<VehicleModel> Models { get; set; }
-
-        public System.Data.Entity.DbSet<VehicleApp.Service.ViewModels.ListVehicleMakeViewModel> ListVehicleMakeViewModels { get; set; }
-
-        public System.Data.Entity.DbSet<VehicleApp.Service.ViewModels.CreateVehicleMakeViewModel> CreateVehicleMakeViewModels { get; set; }
     }
 }
